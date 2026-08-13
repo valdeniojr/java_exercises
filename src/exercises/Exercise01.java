f83ccccc692a57abcdedba6d02445995d20dc6ee
hazzard083 <valdeniogomesdesajunior@gmail.com>
2026-08-12 23:54:06 -0300

    feat: implement calculator operations using switch and Scanner

--- src/exercises/Exercise01.java
+++ src/exercises/Exercise01.java
@@ -7,17 +7,22 @@ public class Exercise01 {
         Scanner sc = new Scanner(System.in);
 
 
-        System.out.print("Digite um número");
+        System.out.print("Digite um número: ");
         int num1 = sc.nextInt();
 
-        System.out.print("Digite outro número");
+        System.out.print("Digite outro número: ");
         int num2 = sc.nextInt();
 
         System.out.print("Digite qual operação deseja fazer (+, -, *, /): ");
         String operation = sc.next();
 
         switch (operation) {
-            case "+" -> System.out.println("O resultado da soma de 1 + 1 é 2");
+            case "+" -> System.out.println("O resultado da soma de " + num1 + " + " + num2 + " é " + (num1 + num2));
+            case "-" -> System.out.println("O resultado da subtração de " + num1 + " - " + num2 + " é " + (num1 - num2));
+            case "*" -> System.out.println("O resultado da multiplicação de " + num1 + " * " + num2 + " é " + (num1 * num2));
+            case "/" -> System.out.println("O resultado da divisão de " + num1 + " / " + num2 + " é " + (num1 / num2));
+            default -> System.out.println("Operação inválida.");
+
         }
     }
 
